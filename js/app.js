b8bd00a04fe622a4ede57a5c709c07a0939a1b64
youpiyoful <yoanfornari@gmail.com>
2018-07-02 16:52:23 +0200

    images à la une + création du modèle de liste d'articles

---
+++ js/app.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) { //spécificité wordpress pour jquerry tout doit être dans cette fonction pour ne pas avoir de conflit avec d'autres library qui utilisent aussi le  $
+
+    $("h1").css("color", "#f00");
+
+});
